0e4ebcd46edd4b1b7cb8e5820de200fee94dccfb
j-hui <j-hui@cs.columbia.edu>
2023-02-01 02:12:49 -0500

    Make temporal behavior a little more interesting

--- lua/fib.lua
+++ lua/fib.lua
@@ -2,26 +2,26 @@ local ssm = require("ssm")
 
 function ssm:pause(d)
   local t = ssm.Channel {}
-  self:after(d, t, 1, 1)
+  self:after(math.max(d, 1), t, 1, 1)
   self:wait(t)
 end
 
-function ssm:sum(r1, r2)
-  self:wait{r1, r2}
-  self:wait(ssm:pause(1))
+function ssm:sum(r1, r2, d)
+  self:wait { r1, r2 }
+  self:wait(ssm:pause(d))
   return r1[1] + r2[1]
 end
 
 function ssm:fib(n)
   if n < 2 then
-    self:wait(ssm:pause(1))
+    self:wait(ssm:pause(n))
     return 1
   end
   local r1 = ssm:fib(n - 1)
   local r2 = ssm:fib(n - 2)
-  local r3 = ssm:sum(r1, r2)
+  local r3 = ssm:sum(r1, r2, n)
 
-  self:wait{r1, r2, r3}
+  self:wait { r1, r2, r3 }
 
   return r3[1]
 end
