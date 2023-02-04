2f03746def17a8d33383274451339fddb39108cc
j-hui <j-hui@cs.columbia.edu>
2023-02-04 00:24:08 -0500

    Begin writing siggen

---
+++ lua/siggen.lua
@@ -0,0 +1,27 @@
+local ssm = require("ssm")
+
+function ssm.sig_gen(half_period)
+  ssm.set_passive()
+  while true do
+
+  end
+end
+
+function ssm.sig_ctl(button1, button2, half_period)
+  while true do
+    ssm.wait(button1, button2)
+    if ssm.last_updated(button1) == ssm.now() then
+      half_period.val = half_period.val * 2
+    else
+      half_period.val = half_period.val / 2
+    end
+  end
+end
+
+function ssm.main()
+  local half_period = ssm.Channel { val = 1000 }
+  local button1, button2
+
+  ssm.sig_gen:spawn(half_period)
+  ssm.sig_ctl:spawn(button1, button2, half_period)
+end
